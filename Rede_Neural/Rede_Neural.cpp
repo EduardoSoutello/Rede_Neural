d20fd9c9200e7f8f7c95b34f1c262394fbf8bda9
J. Eduardo Soutello <eduardosoutello@gmail.com>
2020-06-16 13:24:00 -0300

    Regra delta adcionada

--- Rede_Neural/Rede_Neural.cpp
+++ Rede_Neural/Rede_Neural.cpp
@@ -28,8 +28,9 @@ struct criarNeuronio neural[10000];
 
 void criarNeuronio() { 
     int contador = 0;
+    int k;
     for (i = 0; i < config[0]; i++) {   // para a primeira camada recebe a entrada e alimentação
-        neural->numero = contador;
+        neural[contador].numero = contador;
         *neural[contador].entrada = entradaOrigem[i];
         neural->camada = 1;
         degrauUnitario();
@@ -38,22 +39,21 @@ void criarNeuronio() {
     }
     for (i = 0; i < config[1]; i++) {
        int temp = contador;
-        for (i = 0; i < config[0]; i++) {
-            neural->numero = contador;
+        for (k = 0; k < config[0]; k++) {
+            neural[contador].numero = contador;
             
-            *neural[temp].entrada = neural[i].saida;// nas entradas recebe as saidas da camada 1
+            *neural[temp].entrada = neural[k].saida;// nas entradas recebe as saidas da camada 1
             neural->camada = 2;
             degrauUnitario();
             contador++;
-        }
-        
+        }  
     }
     for (i = 0; i < config[2]; i++) {
         int temp = contador;
-        for (i = 0; i < config[1]; i++) {
-            neural->numero = contador;
+        for (k = 0; k < config[1]; k++) {
+            neural[contador].numero = contador;
 
-            *neural[temp].entrada = neural[i].saida;// nas entradas recebe as saidas da camada 1
+            *neural[temp].entrada = neural[k].saida;// nas entradas recebe as saidas da camada 1
             neural->camada = 3;
             degrauUnitario();
             contador++;
@@ -61,10 +61,10 @@ void criarNeuronio() {
     }
     for (i = 0; i < config[3]; i++) {
         int temp = contador;
-        for (i = 0; i < config[2]; i++) {
-            neural->numero = contador;
+        for (k = 0; k < config[2]; k++) {
+            neural[contador].numero = contador;
 
-            *neural[temp].entrada = neural[i].saida;// nas entradas recebe as saidas da camada 1
+            *neural[temp].entrada = neural[k].saida;// nas entradas recebe as saidas da camada 1
             neural->camada = 4;
             degrauUnitario();
             contador++;
@@ -72,10 +72,10 @@ void criarNeuronio() {
     }
     for (i = 0; i < config[4]; i++) {
         int temp = contador;
-        for (i = 0; i < config[3]; i++) {
-            neural->numero = contador;
+        for (k = 0; k < config[3]; k++) {
+            neural[contador].numero = contador;
 
-            *neural[temp].entrada = neural[i].saida;// nas entradas recebe as saidas da camada 1
+            *neural[temp].entrada = neural[k].saida;// nas entradas recebe as saidas da camada 1
             neural->camada = 5;
             degrauUnitario();
             contador++;
@@ -83,22 +83,25 @@ void criarNeuronio() {
     }
     for (i = 0; i < config[5]; i++) {
         int temp = contador;
-        for (i = 0; i < config[4]; i++) {
-            neural->numero = contador;
+        for (k = 0; k < config[4]; k++) {
+            neural[contador].numero = contador;
 
-            *neural[temp].entrada = neural[i].saida;// nas entradas recebe as saidas da camada 1
+            *neural[temp].entrada = neural[k].saida;// nas entradas recebe as saidas da camada 1
             neural->camada = 6;
             degrauUnitario();
             contador++;
         }
     }
-
 }
 
 
 void treinamentoRede() {
-    printf("Digite a quantidade de camadas! \n");
+    printf("Digite a quantidade de camadas! (maximo 6 camadas)! \n");
     scanf_s("%i", &camada);                                                     // entrada de numero de camadas da rede
+    if (camada > 6){
+        printf("MAXIMO 6 CAMADAS ! \nDigite novamente !\n");
+        scanf_s("%i", &camada);
+    }
     printf("A rede neural sera com %i camada(s)!\n", camada);
     for ( i = 0; i < camada; i++)  {
         printf("Digite a quantidade de neuronios na camada %i!\n",i+1);
@@ -114,15 +117,18 @@ void treinamentoRede() {
 }
 
 void regraDelta() {
-    delta[i] = taxaAprendizagem * *neural->peso;
+    int e1 = 0;             // diferença entre a saida desejada e a saida obtida
+    int o1;                 //saida desejada
+    e1 = o1 - neural->saida;
+    delta[i] = taxaAprendizagem * *neural->peso * e1;
 }
 
 void degrauUnitario() {//função de degrau unitario
     float func01;
     float temp=0;
     int cont=0;
-    for ( i = 0; i < 10000; i++){
-        if (neural->entrada[i]) {
+    for ( j = 0; j < 10000; j++){
+        if (neural->entrada[j]) {
             temp = temp + *neural->entrada;
             cont++;
         }
@@ -144,12 +150,20 @@ void teste() {
     for ( i = 0; i < 100; i++)
     {
         printf_s("%d",i);
-        printf_s(" teste %s \n", neural->numero);
-        printf("%s", neural->saida);
+      
     }
+    int temp = (config[1] + config[1] + config[2] + config[3] + config[4] + config[5]);
+        int i;
+        for ( i = 0; i < temp; i++){
+            printf ("%s ",neural[i].numero);
+            printf("%s ", neural[i].entrada);
+            printf("%s ", neural[i].saida);
+            printf("%s ", neural[i].peso);
+
+        }
 }
 
-int main(){
+int main() {
     setlocale(LC_ALL, "Portuguese");
 
     printf_s("Escolha uma opção\n");
@@ -157,32 +171,32 @@ int main(){
     printf_s("2 : usar rede\n");
     printf_s("3 : teste\n");
 
-    int x,z=0;
+    int x, z = 0;
 
     scanf_s("%i", &x);
-    while (z!=1)
-    {
+
     switch (x)
     {
     case 1:
         treinamentoRede();
+        main();
         break;
     case 2:
         usoRede();
+        main();
         break;
     case 3:
-        for ( i = 0; i < 100; i++)
+        for (i = 0; i < 100; i++)
         {
-            printf("%d",entradaOrigem[i]);
+            printf("%d", entradaOrigem[i]);
         }
-        
+
         teste();
+        system("pause");
         break;
     case 4:
         z = 1;
-    default:
-        break;
-    }
-    system ("pause");
+        system("pause");
+
     }
 }
